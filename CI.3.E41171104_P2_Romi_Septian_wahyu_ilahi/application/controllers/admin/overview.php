64fbae53ac753ee7e2f269310d6bbadcbaa8816d
romi0509 <romi05091998@gmail.com>
2019-03-14 00:50:24 +0700

    romi add

---
+++ CI.3.E41171104_P2_Romi_Septian_wahyu_ilahi/application/controllers/admin/overview.php
@@ -0,0 +1,13 @@
+<?php 
+
+class Overview extends CI_controller{
+	public function __constructor(){
+		parent::__constructor();
+	}
+
+	public function index (){
+		$this->load->view("admin/overview");
+	}
+}
+
+ ?>
