3f957b8db2298742a83fd82d07e0993e8c991967
LaficPrayana1398 <lafic.prayana@gmail.com>
2019-03-27 20:24:46 +0700

    update

--- CI.3.E41171600_P3_Lafic_Imarega_D/application/models/m_data.php
+++ CI.3.E41171600_P3_Lafic_Imarega_D/application/models/m_data.php
@@ -1,7 +1,11 @@
 <?php 
  
 class M_data extends CI_Model{
-	function ambil_data(){
+	function tampil_data(){
 		return $this->db->get('user');
+	}
+ 
+	function input_data($data,$table){
+		$this->db->insert($table,$data);
 	}
 }
---
+++ CI.3.E41171600_P4_Lafic_Imarega_D/application/controllers/Crud.php
@@ -0,0 +1,91 @@
+<?php 
+ 
+ 
+class Crud extends CI_Controller{
+ 
+	function __construct(){
+		parent::__construct();		
+		$this->load->model('m_data');
+		$this->load->helper('url');
+ 
+	}
+ 
+	function index(){
+		$data['user'] = $this->m_data->tampil_data()->result();
+		$this->load->view('v_tampil',$data);
+	}
+
+	function tambah(){
+		$this->load->view('v_input');
+	}
+ 
+	function tambah_aksi(){
+		$nama = $this->input->post('nama');
+		$alamat = $this->input->post('alamat');
+		$pekerjaan = $this->input->post('pekerjaan');
+ 
+		$data = array(
+			'nama' => $nama,
+			'alamat' => $alamat,
+			'pekerjaan' => $pekerjaan
+			);
+		$this->m_data->input_data($data,'user');
+		redirect('Crud/index');
+	}
+
+	function hapus($id){
+		$where = array('id' => $id);
+		$this->m_data->hapus_data($where,'user');
+		redirect('Crud/index');
+	}
+
+	function edit($id){
+		$where = array('id' => $id);
+		$data['user'] = $this->m_data->edit_data($where,'user')->result();
+		$this->load->view('v_edit',$data);
+	}
+
+	function update (){
+		$id	= $this->input->post('id');
+		$nama = $this->input->post('nama');
+		$alamat = $this->input->post('alamat');
+		$pekerjaan = $this->input->post('pekerjaan');
+
+		$data = array(
+			'nama' => $nama,
+			'alamat' => $alamat,
+			'pekerjaan' => $pekerjaan
+			);
+			 
+		$where = array (
+				'id'=> $id
+		);	
+		
+		$this->m_data->update_data($where,$data, 'user');
+		redirect('Crud/index');
+	}
+
+	function aksi_login(){
+		$username = $this->input->post('username');
+		$password = $this->input->post('password');
+		$where = array(
+			'username' => $username,
+			'password' => md5($password)
+			);
+		$cek = $this->m_login->cek_login("admin",$where)->num_rows();
+		if($cek > 0){
+ 
+			$data_session = array(
+				'nama' => $username,
+				'status' => "login"
+				);
+ 
+			$this->session->set_userdata($data_session);
+ 
+			redirect(base_url("admin"));
+ 
+		}else{
+			echo "Username dan password salah !";
+		}
+	}
+}
